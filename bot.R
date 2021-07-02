6ff299b8ede95fe5b2f0ffa5d5edad393a187d79
favstats <fabio.votta@gmail.com>
2021-07-02 22:19:46 +0200

    dont directly reply

--- bot.R
+++ bot.R
@@ -48,7 +48,7 @@ ts_schwabs <- ts %>%
   ungroup() %>% 
   mutate(link = stringr::str_extract(text, "http[^[:space:]]*"),
          schwabtext = stringr::str_replace(schwabtext, "hddb[^[:space:]]*", link),
-         schwabtext = paste0(schwabtext, " (@", screen_name, ")"),
+         # schwabtext = paste0(schwabtext, " (@", screen_name, ")"),
          schwabtext = str_replace(schwabtext, " inna", ":inna"),
          schwabtext = str_replace(schwabtext, " ungern ", " ogern "),
          schwabtext = str_replace(schwabtext, "Ungern ", "Ogern "),
@@ -73,34 +73,11 @@ if(ts_rows==0){
     
     print(.x$schwabtext)
     
-    post_tweet(status = .x$schwabtext, in_reply_to_status_id = .x$status_id, auto_populate_reply_metadata = T)
+    post_tweet(status = .x$schwabtext)
     
     Sys.sleep(10)
     
   })
- 
- print("now retweet the tweets")
- 
- last_tweets <- rtweet::get_timeline(user = "schwabenschau", n = ts_rows) %>% 
-   filter(created_at > lubridate::now() - lubridate::dhours(1))
- 
- if(nrow(last_tweets)==0){
-   
-   print("nothing to retweet")
-   
- }  else {
-   
-   last_tweets %>% 
-     split(1:nrow(.)) %>% 
-     purrr::walk(~{
-       
-       post_tweet(retweet_id = .x$status_id)
-       
-       Sys.sleep(10)
-       
-     })   
-   
- }
  
 
 }
