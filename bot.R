df296ed96ffdd487a75c036249f948e42e9b0656
favstats <fabio.votta@gmail.com>
2021-08-07 19:17:32 +0200

    the timing is different for schwabbot oh no

--- bot.R
+++ bot.R
@@ -206,7 +206,7 @@ print(lubridate::now())
 
 print(lubridate::hour(lubridate::now()))
 
-if(lubridate::hour(lubridate::now()) %in% c(18:22)){
+if(lubridate::hour(lubridate::now()) %in% c(17:22)){
   
   install.packages("telegram.bot")
   
