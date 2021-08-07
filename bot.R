1e4f52ee0837db9e658d897de432acb53c9ca4b3
favstats <fabio.votta@gmail.com>
2021-08-07 18:55:25 +0200

    webshot via source

--- bot.R
+++ bot.R
@@ -11,6 +11,7 @@ library(R6)
 library(processx)
 library(jsonlite)
 library(fastmap)
+library(later)
 
 rvest::read_html("https://github.com/rstudio/chromote/tree/master/R") %>% 
   rvest::html_elements(".Box-row") %>% 
